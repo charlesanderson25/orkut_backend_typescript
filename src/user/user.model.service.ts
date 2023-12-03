32476bf1ef8430357e4202dbca9ebfd997864463
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-03 18:54:02 -0300

    Clear code

--- src/user/user.model.service.ts
+++ src/user/user.model.service.ts
@@ -1,38 +1,5 @@
 import { connectionDataBase } from "../db";
 
-// export async function createUser(data) {
-//   try {
-//     const query = /*SQL*/ `INSERT INTO users(first_name, last_name, avatar, pass_word) VALUES(?, ?, ?, ?)`;
-//     const values = [
-//       data.first_name,
-//       data.last_name,
-//       data.avatar,
-//       data.pass_word,
-//     ];
-
-//     await connectionDataBase.promise().query(query, values);
-
-//     const [lastInsertResult] = await connectionDataBase
-//       .promise()
-//       .query("SELECT LAST_INSERT_ID() as lastInsertId");
-
-//     const lastInsertId = lastInsertResult[0].lastInsertId;
-
-//     const [result] = await connectionDataBase
-//       .promise()
-//       .query("SELECT * FROM users WHERE id = ?", [lastInsertId]);
-
-//     if (result.length === 1) {
-//       return result[0];
-//     } else {
-//       return "Nenhum registro inserido, por favor verifique!";
-//     }
-//   } catch (error) {
-//     console.error("Erro na consulta", error);
-//     throw error;
-//   }
-// }
-
 export async function createUser(data) {
   try {
     const query = `
