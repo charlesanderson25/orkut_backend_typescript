63591c3bfc7cc77dc5308294c95e43ca2e0314b3
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-03 20:17:57 -0300

    Add constructor

--- src/user/user.contoller.ts
+++ src/user/user.contoller.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { listLatestFriends, readUser } from "./user.model.repository";
 import { JsonController, Get, Param } from "routing-controllers";
+import { UserRepository } from "./user.model.repository";
 
 const app = express();
 app.use(cors());
@@ -10,15 +11,21 @@ const userController = express.Router();
 
 @JsonController("/users")
 export class UserController {
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  userRepository: UserRepository;
+
   @Get("/:userId")
   async getById(@Param("userId") userId: number) {
-    const user = await readUser(userId);
+    const user = await this.userRepository.readUser(userId);
     return user;
   }
 
   @Get("/:userId/friends")
   async listLatestFriends(@Param("userId") userId: number) {
-    const friends = await listLatestFriends(userId);
+    const friends = await this.userRepository.listLatestFriends(userId);
     return friends;
   }
 }
