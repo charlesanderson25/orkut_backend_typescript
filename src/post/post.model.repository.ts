576f616cde3d3a7f407bc43edeedad143f1ca4cf
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-07 11:57:21 -0300

    Migration to prisma

--- src/post/post.model.repository.ts
+++ src/post/post.model.repository.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { connectionDataBase } from "../db";
 import { error } from "console";
 import { isAsyncFunction } from "util/types";
+import { prisma } from "../prisma";
 
 //Teste conexão banco de dados **********************************************
 
@@ -66,86 +67,109 @@ export class PostRepository {
   }
 
   async createPost(data: any) {
-    try {
-      const query = "INSERT INTO posts (content, user_id) VALUES (?, ?)";
-      const values = [data.content, data.user_id]; // Certifique-se de que data.user_id seja passado
+    // try {
+    //   const query = "INSERT INTO posts (content, user_id) VALUES (?, ?)";
+    //   const values = [data.content, data.user_id]; // Certifique-se de que data.user_id seja passado
 
-      await connectionDataBase.promise().query(query, values);
+    //   await connectionDataBase.promise().query(query, values);
 
-      const [lastInsertResult] = await connectionDataBase
-        .promise()
-        .query("SELECT LAST_INSERT_ID() as lastInsertId");
+    //   const [lastInsertResult] = await connectionDataBase
+    //     .promise()
+    //     .query("SELECT LAST_INSERT_ID() as lastInsertId");
 
-      const lastInsertId = lastInsertResult[0].lastInsertId;
+    //   const lastInsertId = lastInsertResult[0].lastInsertId;
 
-      const [result] = await connectionDataBase
-        .promise()
-        .query("SELECT * FROM posts WHERE id = ?", [lastInsertId]);
+    //   const [result] = await connectionDataBase
+    //     .promise()
+    //     .query("SELECT * FROM posts WHERE id = ?", [lastInsertId]);
 
-      if (Array.isArray(result) && result.length === 1) {
-        return result[0];
-      } else {
-        return "Nenhum registro inserido, por favor verifique!";
-      }
-    } catch (error) {
-      console.error("Erro na consulta", error);
-      throw error;
-    }
+    //   if (Array.isArray(result) && result.length === 1) {
+    //     return result[0];
+    //   } else {
+    //     return "Nenhum registro inserido, por favor verifique!";
+    //   }
+    // } catch (error) {
+    //   console.error("Erro na consulta", error);
+    //   throw error;
+    // }
+    const nextPost = await prisma.posts.create({
+      data,
+    });
+    return nextPost;
   }
 
   async readPost(id: number) {
-    try {
-      const [rows] = await connectionDataBase.promise().query(
-        /*SQL*/
-        `SELECT * FROM posts WHERE id = ?`,
-        [id]
-      );
+    // try {
+    //   const [rows] = await connectionDataBase.promise().query(
+    //     /*SQL*/
+    //     `SELECT * FROM posts WHERE id = ?`,
+    //     [id]
+    //   );
 
-      if (Array.isArray(rows) && rows.length === 0) {
-        return null; // Retorna null se nenhum registro for encontrado com o ID fornecido
-      }
+    //   if (Array.isArray(rows) && rows.length === 0) {
+    //     return null; // Retorna null se nenhum registro for encontrado com o ID fornecido
+    //   }
 
-      const post = rows[0];
-      return post;
-    } catch (error) {
-      console.error("Erro na consulta:", error);
-      throw error; // Propaga o erro para ser tratado em um nível superior, se necessário
-    }
+    //   const post = rows[0];
+    //   return post;
+    // } catch (error) {
+    //   console.error("Erro na consulta:", error);
+    //   throw error; // Propaga o erro para ser tratado em um nível superior, se necessário
+    // }
+    const post = await prisma.posts.findFirst({
+      where: {
+        id,
+      },
+    });
+    return post;
   }
 
   async updatePost(id: number, data: any) {
-    try {
-      const query = "UPDATE posts SET content = ? WHERE id = ?;";
-      const values = [data.content, id];
+    // try {
+    //   const query = "UPDATE posts SET content = ? WHERE id = ?;";
+    //   const values = [data.content, id];
 
-      const [result] = await connectionDataBase.promise().query(query, values);
+    //   const [result] = await connectionDataBase.promise().query(query, values);
 
-      if ("affectedRows" in result && result.affectedRows === 1) {
-        return "O post foi atualizado com sucesso!";
-      } else {
-        return "Erro ao atualizar o post!";
-      }
-    } catch (error) {
-      console.error("Erro na consulta", error);
-      throw error;
-    }
+    //   if ("affectedRows" in result && result.affectedRows === 1) {
+    //     return "O post foi atualizado com sucesso!";
+    //   } else {
+    //     return "Erro ao atualizar o post!";
+    //   }
+    // } catch (error) {
+    //   console.error("Erro na consulta", error);
+    //   throw error;
+    // }
+    const post = await prisma.posts.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    return post;
   }
 
   async deletePost(id: number) {
-    try {
-      const query = "DELETE FROM posts WHERE id = ?";
-      const values = [id];
+    // try {
+    //   const query = "DELETE FROM posts WHERE id = ?";
+    //   const values = [id];
 
-      const [result] = await connectionDataBase.promise().query(query, values);
-      if ("affectedRows" in result && result.affectedRows === 1) {
-        return "Registro excluído com sucesso!";
-      } else {
-        return "Nenhum registro excluído, verifique o ID.";
-      }
-    } catch (error) {
-      console.error("Erro na consulta", error);
-      throw error;
-    }
+    //   const [result] = await connectionDataBase.promise().query(query, values);
+    //   if ("affectedRows" in result && result.affectedRows === 1) {
+    //     return "Registro excluído com sucesso!";
+    //   } else {
+    //     return "Nenhum registro excluído, verifique o ID.";
+    //   }
+    // } catch (error) {
+    //   console.error("Erro na consulta", error);
+    //   throw error;
+    // }
+    const post = await prisma.posts.delete({
+      where: {
+        id,
+      },
+    });
+    return post;
   }
 
   async listPostComments(id: number) {
