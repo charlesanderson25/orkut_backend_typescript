2c94af804fff726109f90edc222c3bcda2bf98fe
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-09 10:52:00 -0300

    Add new instance UserRepository

--- src/user/user.seed.ts
+++ src/user/user.seed.ts
@@ -1,16 +1,18 @@
 import { faker } from "@faker-js/faker";
-import { createUser } from "./user.model.repository";
+// import { createUser } from "./user.model.repository";
+import { UserRepository } from "./user.model.repository";
 
 const defaultLimit = 100;
 
 async function seedUser() {
+  const userRepository = new UserRepository();
   const limit = Number(process.argv[2] ?? defaultLimit);
   console.log("Iniciando seed...");
   console.log(`Vão ser criados ${limit} users`);
   for (let index = 0; index < limit; index++) {
     const userData = generateUser();
 
-    const user = await createUser(userData);
+    const user = await userRepository.createUser(userData);
     console.log(`Criado usuário de id#${user.id}`);
 
     // await commentSeed(user);
