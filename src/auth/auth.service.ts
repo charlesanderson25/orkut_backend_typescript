6cf94889ac120769fc933ee36b90aec851bf2484
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-13 19:50:35 -0300

    Add JwtService and payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { UserRepository } from "../user/user.model.repository";
 import { SignInDto } from "./dto/sign-in.dto";
 import { UnauthorizedError } from "routing-controllers";
+import { JwtService } from "./jwt.service";
+import { th } from "@faker-js/faker";
 
 export class AuthService {
   constructor() {
     this.userRepository = new UserRepository();
+    this.jwtService = new JwtService();
   }
 
   userRepository: UserRepository;
+  jwtService: JwtService;
 
   async signIn({ email, password }: SignInDto) {
     const maybeUser = await this.userRepository.findByEmail(email);
@@ -20,6 +24,14 @@ export class AuthService {
       throw new UnauthorizedError("E-mail ou senha inválidos");
     }
 
-    return { user: maybeUser };
+    const payload = {
+      id: maybeUser.id,
+      name: `${maybeUser.first_name} ${maybeUser.last_name}`,
+      email: maybeUser.email,
+    };
+
+    const token = this.jwtService.encode(payload);
+
+    return { user: maybeUser, token };
   }
 }
