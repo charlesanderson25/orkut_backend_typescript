a92849630b2ede339db091456290e18961ce41d0
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-16 10:58:04 -0300

    Add @MinLength

--- src/user/dtos/create.user.dto.ts
+++ src/user/dtos/create.user.dto.ts
@@ -1,18 +1,24 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
-  @IsString()
+  @IsString({
+    message: "O nome precisa ser uma string",
+  })
+  @MinLength(2, { message: "O nome precisa ter pelo menos 2 letras" })
   first_name: string;
 
   @IsString()
+  @MinLength(2, { message: "O sobrenome precisa ter pelo menos 2 letras" })
   last_name: string;
 
   @IsString()
+  @IsOptional()
   avatar: string;
 
   @IsString()
+  @MinLength(6, { message: "A senha precisa ter pelo menos 6 caracteres" })
   pass_word: string;
 
-  @IsEmail()
+  @IsEmail(undefined, { message: "O e-mail digitado é inválido" })
   email: string;
 }
