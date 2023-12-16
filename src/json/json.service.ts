20739646bf306bbdfd8cba7920a615b519699028
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-16 15:45:58 -0300

    Add types

--- src/json/json.service.ts
+++ src/json/json.service.ts
@@ -5,24 +5,24 @@ import express from "express";
 const app = express();
 app.use(cors());
 
-export async function createJson(path, data) {
+export async function createJson(path: string, data: any) {
   const dataString = JSON.stringify(data, null, 2);
   await fs.promises.writeFile(path, dataString);
 }
 
-export async function readJson(path) {
+export async function readJson(path: string): Promise<any> {
   const dataBuffer = await fs.promises.readFile(path);
   const dataString = dataBuffer.toString();
   const data = JSON.parse(dataString);
   return data;
 }
 
-export async function updateJson(path, partialJson) {
+export async function updateJson(path: string, partialJson: any) {
   const oldJson = await readJson(path);
   const nextJson = { ...oldJson, ...partialJson };
   await createJson(path, nextJson);
 }
 
-export async function deleteJson(path) {
+export async function deleteJson(path: string) {
   await fs.promises.unlink(path);
 }
