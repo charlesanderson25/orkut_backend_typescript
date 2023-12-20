11f61e343878bb26b8864cf8fd6e879052305775
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-20 18:46:22 -0300

    Changes file

--- src/scrap/scrap.controller.ts
+++ src/scrap/scrap.controller.ts
@@ -11,37 +11,64 @@ import {
 } from "routing-controllers";
 import type { User } from "../user/user.types";
 import { CreateScrapDto } from "./dtos/create-scrap.dto";
-import { UpdatePostDto } from "../post/dtos/update.post.dto";
+import { UpdateScrapDto } from "./dtos/update-scrap.dto";
+import { ScrapService } from "./scrap.service";
 
 @JsonController("/scraps")
 export class ScrapController {
+  constructor() {
+    this.scrapService = new ScrapService();
+  }
+
+  scrapService: ScrapService;
+
   @Authorized()
   @Get("/owner/:userId")
-  async listOwnerScraps(@Param("ownerId") ownerId: number) {}
+  async listOwnerScraps(
+    @Param("ownerId") ownerId: number,
+    @CurrentUser() user: User
+  ) {
+    const scraps = await this.scrapService.listOwnerScraps(ownerId);
+    return scraps;
+  }
 
   @Authorized()
   @Get("/scrapId")
-  async readScrap(@Param("scrapId") scrapId: string) {}
+  async readScrap(@Param("scrapId") scrapId: string) {
+    const scrap = await this.scrapService.readScrap(scrapId);
+    return scrap;
+  }
 
   @Authorized()
   @Post()
   async createScrap(
     @Body() createScrapDto: CreateScrapDto,
     @CurrentUser() user: User
-  ) {}
+  ) {
+    createScrapDto.creatorId = user.id;
+    const scrap = await this.scrapService.createScrap(createScrapDto);
+    return scrap;
+  }
 
   @Put("/scrapId")
   @Authorized()
   async updateScrap(
     @Param("scrapId") scrapId: string,
-    @Body() updateScrapDto: UpdatePostDto,
+    @Body() updateScrapDto: UpdateScrapDto,
     @CurrentUser() user: User
-  ) {}
+  ) {
+    updateScrapDto.creatorId = user.id;
+    const scrap = await this.scrapService.updateScrap(scrapId, updateScrapDto);
+    return scrap;
+  }
 
   @Delete("/:scrapId")
   @Authorized()
   async deleteScrap(
     @Param("scrapId") scrapId: string,
     @CurrentUser() user: User
-  ) {}
+  ) {
+    const scrap = await this.scrapService.deleteScrap(scrapId);
+    return scrap;
+  }
 }
