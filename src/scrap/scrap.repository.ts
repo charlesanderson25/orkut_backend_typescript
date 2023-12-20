e5de13550aa55f8ce73b0667cf2c4c4fe136f89a
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-20 11:59:13 -0300

    Add file

--- src/scrap/scrap.repository.ts
+++ src/scrap/scrap.repository.ts
@@ -1,14 +1,47 @@
-import { CreateScrapDto } from "./dtos/create-scrap";
-import { UpdateScrapDto } from "./dtos/update-scrap";
+import { CreateScrapDto } from "./dtos/create-scrap.dto";
+import { UpdateScrapDto } from "./dtos/update-scrap.dto";
+import { mongodb, Collection, ObjectId } from "../mongodb";
+import type { Scrap } from "./scrap.types";
 
 export class ScrapRepository {
-  async readScrap(id: string) {}
+  constructor() {
+    this.scraps = mongodb.collection("scraps");
+  }
 
-  async listUserScraps(userId: number) {}
+  scraps: Collection<Scrap>;
 
-  async createScrap(createScrapDto: CreateScrapDto) {}
+  async readScrap(id: string) {
+    const scrap = await this.scraps.findOne({
+      _id: new ObjectId(id),
+    });
+    return scrap;
+  }
 
-  async updateScrap(id: string, updateScrapDto: UpdateScrapDto) {}
+  async listOwnerScraps(ownerId: number) {
+    const scraps = await this.scraps.find({
+      ownerId,
+    });
+    return scraps;
+  }
 
-  async deleteScrap(id: string) {}
+  async createScrap(createScrapDto: CreateScrapDto) {
+    const scrap = await this.scraps.insertOne(createScrapDto);
+    return scrap;
+  }
+
+  async updateScrap(id: string, updateScrapDto: UpdateScrapDto) {
+    const scrap = await this.scraps.findOneAndUpdate(
+      {
+        _id: new ObjectId(id),
+      },
+      updateScrapDto
+    );
+  }
+
+  async deleteScrap(id: string) {
+    const scrap = await this.scraps.findOneAndDelete({
+      _id: new ObjectId(id),
+    });
+    return scrap;
+  }
 }
