157583003ad398218583fa7e98c46723b26b7d33
charlesanderson25 <charles_spfc@hotmail.com>
2023-12-20 11:59:21 -0300

    Add file

--- src/scrap/scrap.service.ts
+++ src/scrap/scrap.service.ts
@@ -1,4 +1,6 @@
 import { ScrapRepository } from "./scrap.repository";
+import { CreateScrapDto } from "./dtos/create-scrap.dto";
+import { UpdateScrapDto } from "./dtos/update-scrap.dto";
 
 export class ScrapService {
   constructor() {
@@ -6,4 +8,29 @@ export class ScrapService {
   }
 
   scrapRepository: ScrapRepository;
+
+  async readScrap(id: string) {
+    const scrap = this.scrapRepository.readScrap(id);
+    return scrap;
+  }
+
+  async listOwnerScraps(ownerId: number) {
+    const scraps = await this.scrapRepository.listOwnerScraps(ownerId);
+    return scraps;
+  }
+
+  async createScrap(createScrapDto: CreateScrapDto) {
+    const scrap = await this.scrapRepository.createScrap(createScrapDto);
+    return scrap;
+  }
+
+  async updateScrap(id: string, updateScrapDto: UpdateScrapDto) {
+    const scrap = await this.scrapRepository.updateScrap(id, updateScrapDto);
+    return scrap;
+  }
+
+  async deleteScrap(id: string) {
+    const scrap = await this.scrapRepository.deleteScrap(id);
+    return scrap;
+  }
 }
